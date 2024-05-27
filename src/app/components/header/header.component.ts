2c7eacf5dd43177d7509e34887918f84f4f5ce85
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-05-27 17:29:14 -0500

    colores y mejor diseño para ehader y footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,NzPageHeaderModule],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
